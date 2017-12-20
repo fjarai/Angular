8124ea448d75c053f15a3410ebf9a66bc62bb46c
Frank Jahanarai <fjarai@msn.com>
2017-12-20 05:33:24 -0500

    Adding Seed

---
+++ HAng/systemjs.config.js
@@ -0,0 +1,20 @@
+System.config({
+   transpiler: 'typescript',
+   typescriptOptions: {emitDecoratorMetadata: true},
+   map: {
+       '@angular': 'node_modules/@angular',
+       'rxjs': 'node_modules/rxjs'
+   },
+   paths: {
+       'node_modules/@angular/*': 'node_modules/@angular/*/bundles'
+   },
+   packages: {
+       'app': {main: 'main', defaultExtension: 'ts'},
+       'rxjs': {main: 'Rx'},
+       '@angular/core': {main: 'core.umd.min.js'},
+       '@angular/common': {main: 'common.umd.min.js'},
+       '@angular/compiler': {main: 'compiler.umd.min.js'},
+       '@angular/platform-browser': {main: 'platform-browser.umd.min.js'},
+       '@angular/platform-browser-dynamic': {main: 'platform-browser-dynamic.umd.min.js'}
+   }
+});
